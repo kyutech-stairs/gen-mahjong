90f6573d543cebddfc5c85fec90f948a1f0f833c
TakahiroSono <taka.sono889@gmail.com>
2019-11-16 16:29:03 +0900

    WARNINGを解消

--- app/web-front/vue-pwa/src/views/About.vue
+++ app/web-front/vue-pwa/src/views/About.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="about">
-		<div class="dora">
-			<h3>ドラ</h3>
-			<a href="/input">
-				<div class="dora-box">
-					<!-- <div v-if="input.selected">最初に表示される要素</div>
-  				<div v-else>切り替え後に表示される要素</div> -->
-				</div>
-			</a>
-		</div>
-		<div class="self">
-			<h3>自風</h3>
-			<MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
-		</div>
-		<div class="place">
-			<h3>場風</h3>
-			<MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
-		</div>
-		<div class="first-hai">
-			<h3>初期牌</h3>
-			<a href="/input">
-				<div class="first-hai-box">
-					<!-- <div v-if="input.selected">最初に表示される要素</div>
-  				<div v-else>切り替え後に表示される要素</div> -->
-				</div>
-			</a>
-		</div>
-	</div>
+    <div class="dora">
+      <h3>ドラ</h3>
+      <a href="/input">
+        <div class="dora-box">
+          <!-- <div v-if="input.selected">最初に表示される要素</div>
+          <div v-else>切り替え後に表示される要素</div> -->
+        </div>
+      </a>
+    </div>
+    <div class="self">
+      <h3>自風</h3>
+      <MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
+    </div>
+    <div class="place">
+      <h3>場風</h3>
+      <MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
+    </div>
+    <div class="first-hai">
+      <h3>初期牌</h3>
+      <a href="/input">
+        <div class="first-hai-box">
+          <!-- <div v-if="input.selected">最初に表示される要素</div>
+          <div v-else>切り替え後に表示される要素</div> -->
+        </div>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,51 +35,51 @@ import MyRadio from '@/components/RadioButton.vue';
 export default {
   name: 'about',
   components: {
-    MyRadio
-	},
-	data() {
+    MyRadio,
+  },
+  data() {
     return {
-			input: {
-				selected: true
-			},
+      input: {
+        selected: true,
+      },
       sampleForm: {
-        text: "",
-        radio: "",
-        select: "",
-        textarea: "",
-        checkbox: []
+        text: '',
+        radio: '',
+        select: '',
+        textarea: '',
+        checkbox: [],
       },
       options: [
         {
-          label: "東",
-          value: "a"
+          label: '東',
+          value: 'a',
         },
         {
-          label: "南",
-          value: "b"
+          label: '南',
+          value: 'b',
         },
         {
-          label: "西",
-          value: "c"
-				},
-				{
-          label: "北",
-          value: "d"
-        }
-      ]
+          label: '西',
+          value: 'c',
+        },
+        {
+          label: '北',
+          value: 'd',
+        },
+      ],
     };
   },
   methods: {
     sendForm() {
       // formデータの送信処理
-    }
+    },
   },
 };
 </script>
 
 <style scoped>
 .dora-box {
-	width: 1320px;
+  width: 1320px;
   height: 177px;
   opacity: 0.89;
   border-radius: 10px;
@@ -90,7 +90,7 @@ export default {
   background-color: #4e4e4e;
 }
 .first-hai-box {
-	width: 1320px;
+  width: 1320px;
   height: 177px;
   opacity: 0.89;
   border-radius: 10px;
