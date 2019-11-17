6fa219955dfee1da450050ee9250403598d5b05d
TakahiroSono <taka.sono889@gmail.com>
2019-11-17 16:07:35 +0900

    ドラ表示牌部を作成

--- app/web-front/vue-pwa/src/views/Setting.vue
+++ app/web-front/vue-pwa/src/views/Setting.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="about">
-    <div class="dora">
-      <h3>ドラ</h3>
+  <div class="c-setting">
+
+    <div class="c-setting_dora">
+      <h3 class="c-setting_dora_title">ドラ</h3>
       <a href="/input">
-        <div class="dora-box">
-          <!-- <div v-if="input.selected">最初に表示される要素</div>
-          <div v-else>切り替え後に表示される要素</div> -->
+        <div class="c-setting_dora-box">
+          <img class="c-setting_dora-image" src="../assets/blank_cell.svg" alt="dora">
         </div>
       </a>
     </div>
+
     <div class="self">
       <h3>自風</h3>
       <MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
@@ -17,15 +18,15 @@
       <h3>場風</h3>
       <MyRadio v-model="sampleForm.radio" name="sample-radio" :options="options"></MyRadio>
     </div>
+
     <div class="first-hai">
       <h3>初期牌</h3>
       <a href="/input">
         <div class="first-hai-box">
-          <!-- <div v-if="input.selected">最初に表示される要素</div>
-          <div v-else>切り替え後に表示される要素</div> -->
         </div>
       </a>
     </div>
+
   </div>
 </template>
 
@@ -77,17 +78,31 @@ export default {
 };
 </script>
 
-<style scoped>
-.dora-box {
-  width: 1320px;
-  height: 177px;
-  opacity: 0.89;
-  border-radius: 10px;
-  -webkit-backdrop-filter: blur(30px);
-  backdrop-filter: blur(30px);
-  box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0.16);
-  border: solid 4px #e8e8e8;
-  background-color: #4e4e4e;
+<style lang="scss" scoped>
+.c-setting{
+  &_dora{
+    &_title{
+      display: inline-block;
+      vertical-align: middle;
+      height: 60px;
+      margin-right: 10px;
+    }
+    &-box{
+      display: inline-block;
+      height: 60px;
+      opacity: 0.89;
+      border-radius: 10px;
+      -webkit-backdrop-filter: blur(30px);
+      backdrop-filter: blur(30px);
+      box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0.16);
+      border: solid 4px #e8e8e8;
+      background-color: #4e4e4e;
+    }
+    &-image{
+      object-fit: cover;
+      height: 100%;
+    }
+  }
 }
 .first-hai-box {
   width: 1320px;
