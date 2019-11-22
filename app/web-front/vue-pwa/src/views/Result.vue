3240d470027a892090a9df55965896a8169efaa3
TakahiroSono <taka.sono889@gmail.com>
2019-11-23 06:17:07 +0900

    v1完成

--- app/web-front/vue-pwa/src/views/Result.vue
+++ app/web-front/vue-pwa/src/views/Result.vue
@@ -2,38 +2,34 @@
   <div class="c-result">
     <div class="c-result_document">
       <section class="c-result_document_kei">
-        <div class="c-result_document_kei_title">
+        <h2 class="c-result_document_kei_title">
           {{ agari[resignation].title }}
-        </div>
-        <div class="c-resule_document_kei_content">
+        </h2>
+        <div class="c-result_document_kei_content">
           {{ agari[resignation].overview }}
         </div>
       </section>
       <section>
-        <div>
+        <div class="c-result_document_point_title">
           ポイント
         </div>
-        <div>
+        <div class="c-result_document_point_content">
           {{ agari[resignation].point[point] }}
         </div>
       </section>
-      <section>
-        <div>
+      <section class="c-result_document_recommendation">
+        <div class="c-result_document_recommendation_title">
           オススメ役
         </div>
-        <ul>
-          <li v-for="(value, index) in agari[resignation].yaku" :key="index">
-            <div>
-              {{ value.name }}
-            </div>
-            <div>
-              {{ value.star }}
-            </div>
+        <ul class="c-result_document_recommendation_list">
+          <li class="c-result_document_recommendation_list-item" v-for="(value, index) in agari[resignation].yaku" :key="index">
+            <span class="c-result_document_recommendation_list-item-name">{{ value.name }}</span>
+            <span class="c-result_document_recommendation_list-item-star">{{ value.star }}</span>
           </li>
         </ul>
       </section>
     </div>
-    <a href="./">ホームに戻る</a>
+    <a class="c-result_return" href="./">ホームに戻る</a>
   </div>
 </template>
 <script>
@@ -64,7 +60,6 @@ export default {
     };
   },
   mounted() {
-    console.log(JSON.stringify(InputCards.cards));
     const axiosApi = axios.create({
       headers: {
         'Content-Type': 'application/json',
@@ -81,22 +76,68 @@ export default {
 
 <style lang="scss" scoped>
 .c-result{
-  top: 7.9%;
-  width: 86%;
-  height: 84%;
+  width: 100%;
+  height: 100%;
   position: relative;
-  font-family: Tanuki-Permanent-Marker;
+  background-image: url("/img/background/resultPage.png");
+  background-size: cover;
+  font-family: "Noto Sans JP";
   &_document{
-    height: 72%;
+    $title_font-size: 30px;
+    $content_font-size: 20px;
+    %title-set{
+      font-size: $title_font-size;
+      margin-top: 3%;
+    }
+    box-sizing: border-box;
+    padding: 5% 5%;
     color: #ffffff;
-    // text-align: left;
+    text-align: left;
     &_kei{
       &_title{
+        font-size: 55px;
       }
-      &_document{
-
+      &_content{
+        font-size: $content_font-size;
       }
     }
+    &_point{
+      &_title{
+        @extend %title-set;
+      }
+      &_content{
+        font-size: $content_font-size;
+      }
+    }
+    &_recommendation{
+      height: 20vh;
+      display: block;
+      &_title{
+        @extend %title-set;
+      }
+      &_list{
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        height: 100%;
+        &-item{
+          display: inline-block;
+          width: 48%;
+          &-name{
+            display: inline-block;
+            width: 60%;
+          }
+          &-star{
+            display: inline-block;
+            width: 35%;
+          }
+        }
+      }
+    }
+  }
+  &_return{
+    color: white;
+    font-size: 18px;
   }
 }
 </style>
